ad44ca06773d41b543423a499f9010fc2dac6693
zouhour bellamine <153763862+zouhour13@users.noreply.github.com>
2026-04-11 13:23:50 +0200

    Optimize twoSum function using a hash map
    Refactor twoSum function to use a hash map for improved efficiency.

--- two-sum/solution.py
+++ two-sum/solution.py
@@ -1,6 +1,10 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        for i in range(len(nums)):
-            diff = target - nums[i]
-            if diff in nums[i+1:]:
-                return [i, nums.index(diff, i+1)]
+def twoSum(nums, target):
+    seen = {}
+
+    for i, num in enumerate(nums):
+        diff = target - num
+
+        if diff in seen:
+            return [seen[diff], i]
+
+        seen[num] = i
